3debac009bf46098d2bbbfa9a57bc5c54ee40119
Janum Shah <jshah1006@gmail.com>
2022-10-05 23:04:34 -0400

    adding tracking to bfs

--- launch/bfs/bfs.h
+++ launch/bfs/bfs.h
@@ -10,20 +10,24 @@
 #include <unistd.h>
 #include "../../utils/common.h"
 #include "../../utils/split_huge_page.h"
+#include "../../pin3.7/source/tools/CacheSim/track_access.h"
 
-void init_kernel(int num_nodes, int start_seed, unsigned long *in_index, unsigned long *out_index, unsigned long **in_wl, unsigned long **out_wl, unsigned long **ret) {
-  *ret = (unsigned long *) malloc(sizeof(unsigned long) * num_nodes);
-  *in_wl = (unsigned long *) malloc(sizeof(unsigned long) * num_nodes * 2);
-  *out_wl = (unsigned long *) malloc(sizeof(unsigned long) * num_nodes * 2);
+void init_kernel(int num_nodes, int start_seed, unsigned long *in_index, unsigned long *out_index, unsigned long **in_wl, unsigned long **out_wl, unsigned long **ret)
+{
+  *ret = (unsigned long *)malloc(sizeof(unsigned long) * num_nodes);
+  *in_wl = (unsigned long *)malloc(sizeof(unsigned long) * num_nodes * 2);
+  *out_wl = (unsigned long *)malloc(sizeof(unsigned long) * num_nodes * 2);
 
-  for (unsigned long i = 0; i < num_nodes; i++) {
+  for (unsigned long i = 0; i < num_nodes; i++)
+  {
     (*ret)[i] = -1;
   }
-  
+
   *in_index = 0;
   *out_index = 0;
 
-  for (unsigned long i = start_seed; i < start_seed+SEEDS; i++) {
+  for (unsigned long i = start_seed; i < start_seed + SEEDS; i++)
+  {
     unsigned long index = *in_index;
     *in_index = index + 1;
     (*in_wl)[index] = i;
@@ -31,23 +35,34 @@ void init_kernel(int num_nodes, int start_seed, unsigned long *in_index, unsigne
   }
 }
 
-void kernel(csr_graph G, unsigned long *ret, unsigned long *in_wl, unsigned long *in_index, unsigned long *out_wl, unsigned long *out_index, int tid, int num_threads) {
+void kernel(csr_graph G, unsigned long *ret, unsigned long *in_wl, unsigned long *in_index, unsigned long *out_wl, unsigned long *out_index, int tid, int num_threads)
+{
 
   int hop = 1;
-  while (*in_index > 0) {
+  while (*in_index > 0)
+  {
     printf("-- epoch %d %lu --> push\n", hop, *in_index);
-    for (unsigned long i = tid; i < *in_index; i += num_threads) {
+    for (unsigned long i = tid; i < *in_index; i += num_threads)
+    {
       unsigned long node = in_wl[i];
-      unsigned long start = G.node_array[node]; // starting position
-      unsigned long end = G.node_array[node+1]; // ending position
-      for (unsigned long e = start; e < end; e++) {
-	unsigned long edge_index = G.edge_array[e];
-      	unsigned long v = ret[edge_index]; // PROBLEM
-	if (v == -1) {
-	  ret[edge_index] = hop;
-	  unsigned long index = *out_index;
+      track_access(&in_wl[i]);
+      unsigned long start = G.node_array[node];   // starting position
+      unsigned long end = G.node_array[node + 1]; // ending position
+
+      track_access(&G.node_array[node]); // do i even need these?
+      track_access(&G.node_array[node + 1]);
+
+      for (unsigned long e = start; e < end; e++)
+      {
+        unsigned long edge_index = G.edge_array[e];
+        unsigned long v = ret[edge_index]; // PROBLEM
+        track_access(&ret[edge_index]);
+        if (v == -1)
+        {
+          ret[edge_index] = hop;
+          unsigned long index = *out_index;
           *out_index = *out_index + 1;
-	  out_wl[index] = edge_index;
+          out_wl[index] = edge_index;
         }
       }
     }
@@ -58,6 +73,5 @@ void kernel(csr_graph G, unsigned long *ret, unsigned long *in_wl, unsigned long
     hop++;
     *in_index = *out_index;
     *out_index = 0;
-      
   }
 }
